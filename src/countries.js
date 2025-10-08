615a0a93ed468dd5f1f0e171b4033486f82d11c7
Olya Kovtun <lovkavo@gmail.com>
2025-10-08 22:50:57 +0100

    Add Ukraine

--- src/countries.js
+++ src/countries.js
@@ -39,6 +39,7 @@ const countries = [
   {"country": "Spain", "code": "ES", "locale": "es-ES", "currency": "EUR", "symbol": "€"},
   {"country": "Sweden", "code": "SE", "locale": "sv-SE", "currency": "SEK", "symbol": "kr"},
   {"country": "Switzerland", "code": "CH", "locale": "de-CH", "currency": "CHF", "symbol": "CHF"},
+  {"country": "Ukraine", "code": "UA", "locale": "uk-UA", "currency": "UAH", "symbol": "₴"},
   {"country": "United Arab Emirates", "code": "AE", "locale": "ar-AE", "currency": "AED", "symbol": "د.إ"},
   {"country": "United Kingdom", "code": "GB", "locale": "en-GB", "currency": "GBP", "symbol": "£"},
   {"country": "United States", "code": "US", "locale": "en-US", "currency": "USD", "symbol": "$"}
