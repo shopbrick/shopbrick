9596c6db8ef0d022c398f09052be41a3210b2d96
Oleksandr Avoiants <shhavel@gmail.com>
2026-06-12 15:58:47 +0100

    Fix generate redirects script

--- scripts/generate_redirects.js
+++ scripts/generate_redirects.js
@@ -8,46 +8,43 @@ import {LANGS} from '../src/languages.js';
 
 //
 // Usage:
-//     node scripts/generate_redirects.js
+//     ENV=test node scripts/generate_redirects.js
 //
 
 const pageRoutes = ['about', 'cart', 'blogs', 'policies/privacy-policy', 'policies/refund-policy', 'policies/shipping-policy', 'policies/terms-of-service'];
 const products = getProducts();
 const blogs = getBlogs();
 const buildFolder = env !== 'test' ? 'build' : 'build_test';
-const globalBuildDir = path.join(process.cwd(), buildFolder);
+const buildDir = path.join(process.cwd(), buildFolder);
 const defaultLang = config.defaultLanguage;
 
 async function generateRedirects(language) {
   const isDefaultLang = language === defaultLang;
   const langPref = isDefaultLang ? '' : `${language}/`;
-  const buildDir = isDefaultLang ? globalBuildDir : path.join(globalBuildDir, language);
   const productsDir = path.join(buildDir, 'products');
   const blogsDir = path.join(buildDir, 'blogs');
   await fsExtra.ensureDir(buildDir);
 
   for (const pageRoute of pageRoutes) {
-    await generateRedirectHTMLPage(buildDir, `${langPref}${pageRoute}`);
+    await generateRedirectHTMLPage(`${langPref}${pageRoute}`);
   }
 
-  await fsExtra.ensureDir(productsDir);
   for (const product of products) {
-    await generateRedirectHTMLPage(buildDir, `${langPref}products/${product.pk}`);
+    await generateRedirectHTMLPage(`${langPref}products/${product.pk}`);
   }
 
-  await fsExtra.ensureDir(blogsDir);
   for (const blog of blogs) {
-    await generateRedirectHTMLPage(buildDir, `${langPref}blogs/${blog.handle}`);
+    await generateRedirectHTMLPage(`${langPref}blogs/${blog.handle}`);
   }
 
   console.log(`✅ Redirects generation for language ${language} complete!`);
 }
 
-async function generateRedirectHTMLPage(buildDir, dirPath) {
+async function generateRedirectHTMLPage(dirPath) {
   const fileDir = path.join(buildDir, dirPath);
   await fsExtra.ensureDir(fileDir);
   const filePath = path.join(buildDir, dirPath, 'index.html');
-  // console.log(`📁 Created dir ./${buildFolder}/${dirPath}/`);
+  console.log(`📁 Created dir ./${buildFolder}/${dirPath}/`);
   const pageURL = `https://${config.domain}/${dirPath}.html`;
   const content = `<!DOCTYPE html>
 <meta charset="utf-8">
@@ -55,7 +52,6 @@ async function generateRedirectHTMLPage(buildDir, dirPath) {
 <meta http-equiv="refresh" content="0; URL=${pageURL}">
 <link rel="canonical" href="${pageURL}">`;
   fs.writeFileSync(filePath, content, 'utf-8');
-  // console.log(` ⬅️  Generated: ./${buildFolder}/${dirPath}/index.html`);
   console.log(`⬅️ Redirect: ./${buildFolder}/${dirPath}/index.html`);
 }
 
@@ -64,28 +60,21 @@ for (const language of LANGS) {
 }
 
 async function generateMainLanguageRedirects() {
-  const enDir = path.join(globalBuildDir, defaultLang);
-  await fsExtra.ensureDir(enDir);
-
-  await fsExtra.ensureDir(path.join(enDir, 'policies'));
   for (const pageRoute of pageRoutes) {
-    await generateRootRedirectHTMLPage(pageRoute, enDir);
+    await generateRootRedirectHTMLPage(pageRoute);
   }
 
-  const productsDir = path.join(enDir, 'products');
-  await fsExtra.ensureDir(productsDir);
   for (const product of products) {
-    await generateRootRedirectHTMLPage(`products/${product.pk}`, enDir);
+    await generateRootRedirectHTMLPage(`products/${product.pk}`);
   }
 
-  const blogsDir = path.join(enDir, 'blogs');
-  await fsExtra.ensureDir(blogsDir);
   for (const blog of blogs) {
-    await generateRootRedirectHTMLPage(`blogs/${blog.handle}`, enDir);
+    await generateRootRedirectHTMLPage(`blogs/${blog.handle}`);
   }
 }
 
-async function generateRootRedirectHTMLPage(p, enDir) {
+async function generateRootRedirectHTMLPage(p) {
+  const enDir = path.join(buildDir, defaultLang);
   const pageURL = `https://${config.domain}/${p}.html`;
 
   const content = `<!DOCTYPE html>
@@ -94,14 +83,15 @@ async function generateRootRedirectHTMLPage(p, enDir) {
 <meta http-equiv="refresh" content="0; URL=${pageURL}">
 <link rel="canonical" href="${pageURL}">`;
 
+  await fsExtra.ensureDir(path.join(enDir, p));
+
   const f1 = path.join(enDir, `${p}.html`);
   fs.writeFileSync(f1, content, 'utf-8');
-  console.log(`⬅️ Redirect: /${defaultLang}/${p}.html`);
 
-  await fsExtra.ensureDir(path.join(enDir, p));
   const f2 = path.join(enDir, p, 'index.html');
   fs.writeFileSync(f2, content, 'utf-8');
-  // console.log(`⬅️ Redirect: /${defaultLang}/${p}/index.html`);
+
+  console.log(`⬅️ Redirect from: /${defaultLang}/${p}.html`);
 }
 
 generateMainLanguageRedirects().catch(console.error);
